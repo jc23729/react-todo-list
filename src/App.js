fec49360ef084fc0e8f98d3077028c2d0b1a63ae
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-22 18:10:01 -0400

    setup custom hooks, todos, setTodos
    setup custom hooks, setup form

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+  //usestate custom hooks
+  //we have access of variable we can access setTodos is our setter function
+  const [todos, setTodos] = React.useState([])
+  const [todo, setTodo] = React.useState("")
   return (
     <div className="App">
-      
+      <form>
+        
+      </form>
     </div>
   );
 }
