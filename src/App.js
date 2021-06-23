4474bd00e3e66a9a602be0cfddd9041bf51ce869
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-22 22:20:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react from 'react'
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,8 +9,17 @@ function App() {
   const [todo, setTodo] = React.useState("")
   return (
     <div className="App">
-      <form>
-        
+      {/* onSubmit will run any time a button with submit is clicked */}
+      <form onSubmit={}>
+        {/*we pass in an arrow function and pass text we want to set, e is simply a form event*/}
+        {/*e= event or form event, target is the input we created, value is the value of that input*/}
+        {/* value={todo} is two way data binding, make sure the value is set to todo */}
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
       </form>
     </div>
   );
