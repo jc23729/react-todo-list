99f1d5abd35f0e2ea3dc93d54e9f06df855fac69
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-22 22:40:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,28 @@ function App() {
   //we have access of variable we can access setTodos is our setter function
   const [todos, setTodos] = React.useState([])
   const [todo, setTodo] = React.useState("")
+
+
+
+
+  function handleSubmit(e) {
+    // if we dont add eventpreventDefault, page wont refresh when we add todo item
+    e.preventDefault()
+    const newTodo = {
+      //we need a unique value here
+      Id: new Date().getTime(),
+      //matched up with our todo hook we did earlier
+      text: todo,
+      completed: false
+    }
+  }
+
+
+
   return (
     <div className="App">
       {/* onSubmit will run any time a button with submit is clicked */}
-      <form onSubmit={}>
+      <form onSubmit={handleSubmit}>
         {/*we pass in an arrow function and pass text we want to set, e is simply a form event*/}
         {/*e= event or form event, target is the input we created, value is the value of that input*/}
         {/* value={todo} is two way data binding, make sure the value is set to todo */}
